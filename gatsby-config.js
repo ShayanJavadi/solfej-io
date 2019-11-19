f8ea827c67854b27e525f26db22803c5cddadbac
ShayanJavadi <shayanjavadi1375@gmail.com>
2019-11-19 15:08:21 -0600

    Ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,27 +5,16 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
       {
           resolve: `gatsby-plugin-google-analytics`,
           options: {
               // The property ID; the tracking code won't be generated without it
               trackingId: "UA-150995686-1",
-              // Defines where to place the tracking script - `true` in the head and `false` in the body
-              head: false,
-              // Setting this parameter is optional
+              head: true,
               anonymize: true,
-              // Setting this parameter is also optional
-              respectDNT: true,
-
-              // Delays sending pageview hits on route update (in milliseconds)
-              pageTransitionDelay: 0,
-
-              // Any additional optional fields
-              sampleRate: 5,
-              siteSpeedSampleRate: 10,
           },
       },
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
