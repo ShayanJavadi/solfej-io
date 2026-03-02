13974340353d41171c050694f447984aff296812
sjtouu <shayanjavadi1375@gmail.com>
2026-03-02 12:58:04 -0600

    scrubby scvrubby

--- src/components/ShayanJavadi.js
+++ src/components/ShayanJavadi.js
@@ -1,35 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-const ShayanJavadi = (alt) => {
-    const data = useStaticQuery(graphql`
-    query {
-      shayanImage: file(relativePath: { eq: "shayan-javadi.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 700) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-    }
-  `)
-
-
-
-
-    return <Img fluid={data.shayanImage.childImageSharp.fluid} alt={alt} />
+const ShayanJavadi = () => {
+    return (
+        <div
+            style={{
+                width: 55,
+                height: 55,
+                borderRadius: "100%",
+                backgroundColor: "#d1d5db",
+            }}
+        />
+    )
 }
 
 export default ShayanJavadi
